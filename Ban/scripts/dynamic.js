20b644be4c924b632811ece3f2e26531fdd78a6e
Armaan Khaitan <khaitanarmaan@gmail.com>
2026-01-03 21:01:28 +0000

    sdf

--- Ban/scripts/dynamic.js
+++ Ban/scripts/dynamic.js
@@ -1,1 +1,11 @@
-const overallBox = document.getElementsByClassName("containerBox")
+const overallBox = document.getElementById("containerBox");
+
+// Create top of piano
+const topOfPiano = document.createElement("div");
+topOfPiano.id = "topPianoLine";
+topOfPiano.style.position = "absolute";
+topOfPiano.style.width = "100%";
+topOfPiano.style.height = "0.5%";
+topOfPiano.style.top = "70%"
+topOfPiano.style.backgroundColor = "black";
+overallBox.appendChild(topOfPiano);
